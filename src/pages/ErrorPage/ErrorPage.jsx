0a2c1e1b38118f78ba6ced1e0545005c40ced337
KosarMyatyi <nespovityi_oleg@mail.ru>
2023-03-31 18:10:31 +0300

    small ref;

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,16 +1,12 @@
 import Styles from './ErrorPage.module.css'
-import Error from '../../assets/img/Error.svg'
 import TheDeathStar from '../../assets/img/TheDeathStar.svg'
 import { Link } from "react-router-dom";
 
 export function ErrorPage() {
     return (
         <div className={Styles.errorPage}>
-            {/* <div className={Styles.error}>
-                <img src={Error} alt='Error' />
-            </div> */}
-            <div className={Styles.DeadthStar} >
-                <img src={TheDeathStar} alt='TheDeathStar' />
+            <div className={Styles.error}>
+                <img className={Styles.DeadthStar} src={TheDeathStar} alt='TheDeathStar' />
             </div>
             <div className={Styles.buttonContainer}>
                 <Link to='/' className={Styles.button}>Return</Link>
